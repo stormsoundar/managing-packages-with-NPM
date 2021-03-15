4c10664e1ea72381208a792ed6042c72aa084a9a
RB <soundarit1995@gmail.com>
2021-03-15 19:14:34 +0530

    Add Comment for every Operation

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-// app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'));
 
 // app.route('/_api/package.json')
 //   .get(function(req, res, next) {
