895e63536e2d29f2edc697d55faf00ba076f645a
RB <soundarit1995@gmail.com>
2021-03-15 19:11:12 +0530

    Add Comment for every Operation

--- server.js
+++ server.js
@@ -24,36 +24,36 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use('/public', express.static(process.cwd() + '/public'));
+// app.use('/public', express.static(process.cwd() + '/public'));
 
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
+// app.route('/_api/package.json')
+//   .get(function(req, res, next) {
+//     console.log('requested');
+//     fs.readFile(__dirname + '/package.json', function(err, data) {
+//       if(err) return next(err);
+//       res.type('txt').send(data.toString());
+//     });
+//   });
   
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
+// app.route('/')
+//     .get(function(req, res) {
+// 		  res.sendFile(process.cwd() + '/views/index.html');
+//     })
 
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
+// // Respond not found to all the wrong routes
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.type('txt').send('Not found');
+// });
 
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
-})
+// // Error Middleware
+// app.use(function(err, req, res, next) {
+//   if(err) {
+//     res.status(err.status || 500)
+//       .type('txt')
+//       .send(err.message || 'SERVER ERROR');
+//   }  
+// })
 
 app.listen(process.env.PORT || PORT, function () {
   console.log('Node.js listening ...' + PORT);
