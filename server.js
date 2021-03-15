731fdf107b5bff10ad6358193822b871f136dec3
RB <soundarit1995@gmail.com>
2021-03-15 19:12:42 +0530

    Add Comment for every Operation

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ var app = express();
 const PORT = 3004;
 const myApp = require("./myApp")
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
+// if (!process.env.DISABLE_XORIGIN) {
+//   app.use(function(req, res, next) {
+//     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+//     var origin = req.headers.origin || '*';
+//     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+//          console.log(origin);
+//          res.setHeader('Access-Control-Allow-Origin', origin);
+//          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     }
+//     next();
+//   });
+// }
 
 // app.use('/public', express.static(process.cwd() + '/public'));
 
