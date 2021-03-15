6427d9317bca1df5873f51bcd180ab14f1ccb1ac
RB <soundarit1995@gmail.com>
2021-03-15 19:15:21 +0530

    Add Comment for every Operation

--- server.js
+++ server.js
@@ -26,19 +26,19 @@ if (!process.env.DISABLE_XORIGIN) {
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-// app.route('/_api/package.json')
-//   .get(function(req, res, next) {
-//     console.log('requested');
-//     fs.readFile(__dirname + '/package.json', function(err, data) {
-//       if(err) return next(err);
-//       res.type('txt').send(data.toString());
-//     });
-//   });
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
   
-// app.route('/')
-//     .get(function(req, res) {
-// 		  res.sendFile(process.cwd() + '/views/index.html');
-//     })
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
 
 // // Respond not found to all the wrong routes
 // app.use(function(req, res, next){
